a8e68397aa4237c946621f2273316a47dfa4ad3b
«Elena <e.sorokina761@gmail.com>
2023-06-14 11:56:36 +0400

    add Homework_10

---
+++ Homework_10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int Decrease(int Number)
+{
+    int num1 = Number/100;
+    int num2 = Number%10;
+    int num3 = (Number - (num1*100+num2))/10;
+    return(num3);
+}
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Decrease(Num));
