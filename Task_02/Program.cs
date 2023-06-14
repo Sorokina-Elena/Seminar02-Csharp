fd4302b33ef047a065614c2079b8a1d7b3315871
«Elena <e.sorokina761@gmail.com>
2023-06-14 11:04:18 +0400

    add dask_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int Decrease(int Number)
+{
+    Console.WriteLine(Number + "=> ");
+
+    int firstNum = Number/100;
+    int secondNum = Number%10;
+    int thirdNum = firstNum*10 + secondNum;
+    return(thirdNum);
+}
+int NumberRand = new Random().Next(100,1000);
+int result = Decrease(NumberRand);
+Console.WriteLine(result);
+
