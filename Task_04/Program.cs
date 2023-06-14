c7a4978d88cf1c84bd9be716da1ca336332b36af
«Elena <e.sorokina761@gmail.com>
2023-06-14 11:05:30 +0400

    add task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+7 и 23.
+
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+string Decrease(int Number)
+{
+    int num1 = 23;
+    int num2 = 7;
+    string answer = Number%num1==0 && Number%num2==0 ? "да" : "нет";
+    return(answer);
+}
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+string result = Decrease(Num);
+Console.WriteLine(Decrease(Num));
